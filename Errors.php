4577c5e4eddec12e96b8e5ae664c3d32d4f40a2d
goverfl0w <officialdeltadev@gmail.com>
2021-02-23 02:27:59 -0500

    API Implements.
    - POST function is now implemented and functioning.
    - GET class is finished with all of the requirements added in.
    
    TO CONSIDER:
    ... Maybe should consider adding an Error assoc. array for rate limits?

--- Errors.php
+++ Errors.php
@@ -1,27 +1,8 @@
 <?php
 
 /*
-  MIT License
-
-  Copyright (c) 2021-present fl0w
-
-  Permission is hereby granted, free of charge, to any person obtaining a copy
-  of this software and associated documentation files (the "Software"), to deal
-  in the Software without restriction, including without limitation the rights
-  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-  copies of the Software, and to permit persons to whom the Software is
-  furnished to do so, subject to the following conditions:
-
-  The above copyright notice and this permission notice shall be included in all
-  copies or substantial portions of the Software.
-
-  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-  SOFTWARE.
+  Copyright (c) 2021-present fl0w, licensed under the MIT License.
+  Refer to the LICENSE document in the GitHub for the full license.
 */
 
 // We want to disable PHP internal warnings for the time being.
@@ -92,7 +73,7 @@ final class TopError implements Exceptions {
 
     $this->error["desc"] = $err; # lazy add for code shortening.
 
-    if($this->write_perms) die("[ERROR] {$name}: {$err}");
+    if($this->write_perms) die("[TopPHP] Error {$name}: {$err}");
   }
 }
 
--- TopPHP.php
+++ TopPHP.php
@@ -1,27 +1,8 @@
 <?php
 
 /*
-  MIT License
-
-  Copyright (c) 2021-present fl0w
-
-  Permission is hereby granted, free of charge, to any person obtaining a copy
-  of this software and associated documentation files (the "Software"), to deal
-  in the Software without restriction, including without limitation the rights
-  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-  copies of the Software, and to permit persons to whom the Software is
-  furnished to do so, subject to the following conditions:
-
-  The above copyright notice and this permission notice shall be included in all
-  copies or substantial portions of the Software.
-
-  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-  SOFTWARE.
+  Copyright (c) 2021-present fl0w, licensed under the MIT License.
+  Refer to the LICENSE document in the GitHub for the full license.
 */
 
 REQUIRE "Errors.php";
@@ -110,7 +91,8 @@ class TopGG implements BaseStruct {
     // As long as we're not rate limited, make the HTTP request.
     if($this->RESP !== 429) {
       $this->headers["http"] = [
-        "header" => "Authorization: {$this->token}\nContent-type: application/x-www-form-urlencoded",
+        "header"  => "Authorization: {$this->token}\r\n" .
+                    "Content-type: application/json\r\n",
         "method"  => $method,
         "content" => http_build_query($parameters)
       ];
@@ -190,7 +172,7 @@ class GET extends TopGG {
       ]
     );
 
-    return $this->result[0];
+    if($this->RESP == 200) return $this->result[0];
   }
 
   public function bot(int $id) {
@@ -202,7 +184,7 @@ class GET extends TopGG {
       "GET", "bot/{$id}"
     );
 
-    return $this->result;
+    if($this->RESP == 200) return $this->result;
   }
 
   public function votes(int $id) {
@@ -214,7 +196,7 @@ class GET extends TopGG {
       "GET", "bots/{$id}/votes"
     );
 
-    return $this->result[0];
+    if($this->RESP == 200) return $this->result[0];
   }
 
   public function vote(int $bot_id, int $user_id) {
@@ -229,7 +211,7 @@ class GET extends TopGG {
       ]
     );
 
-    return $this->request["voted"];
+    if($this->RESP == 200) return $this->request["voted"];
   }
 
   public function stats(int $id) {
@@ -241,7 +223,17 @@ class GET extends TopGG {
       "GET", "bots/{$id}/stats"
     );
 
-    return $this->request;
+    if($this->RESP == 200) return $this->request;
+  }
+
+  public function user(int $id) {
+    /*
+      Retrieves information about a particular user.
+    */
+
+    $this->result = $this->CALL(
+      "GET", "user/{$id}"
+    );
   }
 }
 
@@ -256,10 +248,28 @@ class POST extends TopGG {
     $api->POST->votes; # ¯\_(ツ)_/¯
   */
 
+  // Let this represent an instance for values.
+  private $result;
+
   public function __construct(string $auth) {
     /*
       Declare variables through our class construction.
     */
+  }
+
+  public function stats(int $id, int $servers, array $contents) {
+    /*
+      Sends stats about the bot.
+    */
+
+    $this->result = $this->CALL(
+      "POST", "bots/{$id}/stats", $contents .
+      [
+        "server_count" => $servers
+      ]
+    );
+
+    if($this->RESP == 200) return $this->request;
   }
 }
 
